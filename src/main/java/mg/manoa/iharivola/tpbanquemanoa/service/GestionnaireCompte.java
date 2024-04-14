ceebbebe890ec9ecc0d40696a6927dde9f1368df
manoaiharivolaa <manoaiharivola@gmail.com>
2024-04-14 23:39:21 +0300

    7.6. Écriture des pages JSF et des backing beans associés - Transfert d'argent d'un compte à l'autre - Déposer ou retirer de l'argent sur un compte

--- src/main/java/mg/manoa/iharivola/tpbanquemanoa/service/GestionnaireCompte.java
+++ src/main/java/mg/manoa/iharivola/tpbanquemanoa/service/GestionnaireCompte.java
@@ -56,10 +56,8 @@ public class GestionnaireCompte {
     @Transactional
     public void transferer(CompteBancaire source, CompteBancaire destination,
             int montant) {
-        source.retirer(montant);
-        destination.deposer(montant);
-        update(source);
-        update(destination);
+        this.retirer(source, montant);
+        this.deposer(destination, montant);
     }
 
     @Transactional
@@ -69,5 +67,29 @@ public class GestionnaireCompte {
 
     public CompteBancaire findById(long id) {
         return em.find(CompteBancaire.class, id);
+    }
+
+    /**
+     * Dépôt d'argent sur un compte bancaire.
+     *
+     * @param compteBancaire
+     * @param montant
+     */
+    @Transactional
+    public void deposer(CompteBancaire compteBancaire, int montant) {
+        compteBancaire.deposer(montant);
+        update(compteBancaire);
+    }
+
+    /**
+     * Retrait d'argent sur un compte bancaire.
+     *
+     * @param compteBancaire
+     * @param montant
+     */
+    @Transactional
+    public void retirer(CompteBancaire compteBancaire, int montant) {
+        compteBancaire.retirer(montant);
+        update(compteBancaire);
     }
 }
