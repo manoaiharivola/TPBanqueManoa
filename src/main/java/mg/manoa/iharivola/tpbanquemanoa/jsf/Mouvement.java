6a1af8c7b79e7d8237e52b73c3d508fa36a9cad7
manoaiharivolaa <manoaiharivola@gmail.com>
2024-04-26 01:40:30 +0300

    TP5 - 11. Prise en compte des accès concurrents - Ajout d'un attribut dans l'entité CompteBancaire pour le numéro de version

--- src/main/java/mg/manoa/iharivola/tpbanquemanoa/jsf/Mouvement.java
+++ src/main/java/mg/manoa/iharivola/tpbanquemanoa/jsf/Mouvement.java
@@ -12,6 +12,7 @@ import jakarta.faces.validator.ValidatorException;
 import jakarta.inject.Named;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
+import jakarta.persistence.OptimisticLockException;
 import java.io.Serializable;
 import mg.manoa.iharivola.tpbanquemanoa.entity.CompteBancaire;
 import mg.manoa.iharivola.tpbanquemanoa.jsf.util.Util;
@@ -106,12 +107,18 @@ public class Mouvement implements Serializable {
     }
 
     public String enregistrerMouvement() {
-        if (typeMouvement.equals("ajout")) {
-            gestionnaireCompte.deposer(compte, montant);
-        } else {
-            gestionnaireCompte.retirer(compte, montant);
+        try {
+            if (typeMouvement.equals("ajout")) {
+                gestionnaireCompte.deposer(compte, montant);
+            } else {
+                gestionnaireCompte.retirer(compte, montant);
+            }
+            Util.addFlashInfoMessage("Mouvement enregistré sur le compte de " + compte.getNom());
+            return "listeComptes?faces-redirect=true";
+        } catch (OptimisticLockException ex) {
+            Util.messageErreur("Le compte de " + compte.getNom()
+                    + " a été modifié ou supprimé par un autre utilisateur !");
+            return null; // pour rester sur la page s'il y a une exception
         }
-        Util.addFlashInfoMessage("Mouvement enregistré sur le compte de " + compte.getNom());
-        return "listeComptes?faces-redirect=true";
     }
 }
